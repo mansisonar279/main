476e3a76b72869c92be5e04babcdf3a78595e6fb
mansisonar279 <mansirsonar78@gmail.com>
2025-05-14 15:36:59 +0530

    save

---
+++ Main.java
@@ -0,0 +1,26 @@
+ class BankCustomer {
+    private String name;
+    private String accountNumber;
+    private int age;
+
+    public BankCustomer(String name, String accountNumber, int age) {
+        this.name = name;
+        this.accountNumber = accountNumber;
+        this.age = age;
+    }
+    
+    public void displayCustomerInfo() {
+        System.out.println("Customer Name: " + name);
+        System.out.println("Account Number: " + accountNumber);
+        System.out.println("Customer Age: " + age);
+    }
+}
+public class Main{
+        
+        public static void main(String[] args) {
+           
+            
+        }
+    }
+ 
+   
